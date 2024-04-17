219503cc2da03b44afc510b29b389956b2a47202
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-04-17 15:34:18 +0200

    ADD Snack2

--- csharp-typesystem-snacks/Program.cs
+++ csharp-typesystem-snacks/Program.cs
@@ -8,80 +8,143 @@
             // Snack 1
             // -------
 
-            int number1 = 0;
-            int number2 = 0;
-
-            // Recuperiamo il primo numero, si fa il ciclo finche non riusciamo assegnare a number1 un valore numerico.
-            while (true)
             {
-                // Chiediamo inserire un numero al utente
-                Console.WriteLine("Inserisci il primo numero:");
+                int number1 = 0;
+                int number2 = 0;
 
-                // Salviamo input 
-                string userInput = Console.ReadLine();
-
-                // Proviamo ad assegnare a number1 il valore del userInput, se viene eseguito assegnazione - si esce dal ciclo tramite break.
-                try
+                // Recuperiamo il primo numero, si fa il ciclo finche non riusciamo assegnare a number1 un valore numerico.
+                while (true)
                 {
-                    //number1 = int.Parse(userInput);
-                    number1 = Convert.ToInt32(userInput);
-                    break;
+                    // Chiediamo inserire un numero al utente
+                    Console.WriteLine("Inserisci il primo numero:");
+
+                    // Salviamo input 
+                    string userInput = Console.ReadLine();
+
+                    // Proviamo ad assegnare a number1 il valore del userInput, se viene eseguito assegnazione - si esce dal ciclo tramite break.
+                    try
+                    {
+                        //number1 = int.Parse(userInput);
+                        number1 = Convert.ToInt32(userInput);
+                        break;
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Errore di formato. Inserisci un numero valido, NON puo contenere le lettere o simboli!.");
+                    }
                 }
-                catch (FormatException)
+
+                // Recuperiamo il secondo numero, si fa il ciclo finche non riusciamo assegnare a number2 un valore numerico.
+                while (true)
                 {
-                    Console.WriteLine("Errore di formato. Inserisci un numero valido, NON puo contenere le lettere o simboli!.");
+                    // Chiediamo inserire un numero al utente
+                    Console.WriteLine("Inserisci il secondo numero:");
+
+                    // Salviamo input 
+                    string userInput = Console.ReadLine();
+
+                    // Proviamo ad assegnare a number1 il valore del userInput, se viene eseguito assegnazione - si esce dal ciclo tramite break.
+                    try
+                    {
+                        //number2 = int.Parse(userInput);
+                        number2 = Convert.ToInt32(userInput);
+                        break;
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Errore di formato. Inserisci un numero valido, NON puo contenere le lettere o simboli!.");
+                    }
+                }
+
+                // Stampiamo i resultati 
+                Console.WriteLine("I numeri inseriti sono:");
+                Console.WriteLine($"number1 = {number1}");
+                Console.WriteLine($"number2 = {number2}");
+
+                // Stabiliamo il numero maggiore
+                int maxNumber = Math.Max(number1, number2);
+
+                // Stampiamo la risposta tramite if esle
+                //if (maxNumber == number1)
+                //{
+                //    Console.WriteLine("Number1 è maggiore");
+                //} 
+                //else
+                //{
+                //    Console.WriteLine("Number2 è maggiore");
+                //}
+
+                // Stampiamo la risposta tramite switch
+                switch (maxNumber)
+                {
+                    case int value when value == number1:
+                        Console.WriteLine("Number1 è maggiore");
+                        break;
+                    case int value when value == number2:
+                        Console.WriteLine("Number2 è maggiore");
+                        break;
                 }
             }
 
-            // Recuperiamo il secondo numero, si fa il ciclo finche non riusciamo assegnare a number2 un valore numerico.
-            while (true)
+            // -------
+            // Snack 2
+            // -------
+
             {
-                // Chiediamo inserire un numero al utente
-                Console.WriteLine("Inserisci il secondo numero:");
+                string word1, word2;
 
-                // Salviamo input 
-                string userInput = Console.ReadLine();
-
-                // Proviamo ad assegnare a number1 il valore del userInput, se viene eseguito assegnazione - si esce dal ciclo tramite break.
-                try
+                // Recuperiamo la prima parola, si fa il ciclo finche non riusciamo assegnare a word1 il valore correto.
+                while (true)
                 {
-                    //number2 = int.Parse(userInput);
-                    number2 = Convert.ToInt32(userInput);
-                    break;
+                    // Chiediamo inserire la prima parola all'utente
+                    Console.WriteLine("Inserisci la prima parola:");
+
+                    // Salviamo input 
+                    string userInput = Console.ReadLine();
+
+                    // Verifichiamo se la stringa non è vuota e contiene almeno un carattere alfabetico
+                    if (!string.IsNullOrEmpty(userInput) && userInput.Any(char.IsLetter))
+                    {
+                        word1 = userInput;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Errore. Inserisci una parola valida (senza numeri o simboli). La parola deve contenere almeno una lettera.");
+                    }
                 }
-                catch (FormatException)
+
+                // Recuperiamo la seconda parola, si fa il ciclo finche non riusciamo assegnare a word2 il valore correto.
+                while (true)
                 {
-                    Console.WriteLine("Errore di formato. Inserisci un numero valido, NON puo contenere le lettere o simboli!.");
+                    // Chiediamo inserire la seconda parola all'utente
+                    Console.WriteLine("Inserisci la seconda parola:");
+
+                    // Salviamo input 
+                    string userInput = Console.ReadLine();
+
+                    // Verifichiamo se la stringa non è vuota e contiene almeno un carattere alfabetico
+                    if (!string.IsNullOrEmpty(userInput) && userInput.Any(char.IsLetter))
+                    {
+                        word2 = userInput;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Errore. Inserisci una parola valida (senza numeri o simboli). La parola deve contenere almeno una lettera.");
+                    }
                 }
-            }
 
-            // Stampiamo i resultati 
-            Console.WriteLine("I numeri inseriti sono:");
-            Console.WriteLine($"number1 = {number1}");
-            Console.WriteLine($"number2 = {number2}");
+                // Verifichiamo la lunghezza di ogni parola
+                int word1Length = word1.Length;
+                int word2Length = word2.Length;
 
-            // Stabiliamo il numero maggiore
-            int maxNumber = Math.Max(number1, number2);
+                string wordShorter = (word1Length > word2Length) ? word2 : word1;
+                string wordLonger = (word1Length > word2Length) ? word1 : word2;
 
-            // Stampiamo la risposta tramite if esle
-            //if (maxNumber == number1)
-            //{
-            //    Console.WriteLine("Number1 è maggiore");
-            //} 
-            //else
-            //{
-            //    Console.WriteLine("Number2 è maggiore");
-            //}
-
-            // Stampiamo la risposta tramite switch
-            switch (maxNumber)
-            {
-                case int value when value == number1:
-                    Console.WriteLine("Number1 è maggiore");
-                    break;
-                case int value when value == number2:
-                    Console.WriteLine("Number2 è maggiore");
-                    break;
+                // Stampiamo il risultato
+                Console.WriteLine($"La parola piu corta è {wordShorter}");
+                Console.WriteLine($"La parola piu lunga è {wordLonger}");
             }
         }
     }
